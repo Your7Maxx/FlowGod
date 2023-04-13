834e9f2140a7897f2e544bff51e7c5f3f77a00d8
Maxx <56012056+Your7Maxx@users.noreply.github.com>
2023-04-13 21:05:58 +0800

    Update test_https.py

--- test/test_https.py
+++ test/test_https.py
@@ -4,6 +4,7 @@ sys.path.append('./user/')
 sys.path.append('./utils/')
 import user_https
 
+print(1)
 global_arg = user_https.Global("all","ens3","/lib/x86_64-linux-gnu/libssl.so.3")
 
 user_https.run(global_arg)
